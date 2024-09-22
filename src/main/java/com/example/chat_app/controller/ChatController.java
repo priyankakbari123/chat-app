419e7e71ba270bd2b6b8def02b1cf4a3ca498d3c
priyankakbari123 <akbaripriyank123@gmail.com>
2024-09-22 17:48:33 +0530

    Websocket added

--- src/main/java/com/example/chat_app/controller/ChatController.java
+++ src/main/java/com/example/chat_app/controller/ChatController.java
@@ -4,6 +4,10 @@ import com.example.chat_app.config.KafkaConstants;
 import com.example.chat_app.domain.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,7 @@ public class ChatController {
     @Autowired
     KafkaTemplate<String, Message> kafkaTemplate;
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "/message/send", produces = "application/json")
     public void sendMessage(@RequestBody Message message) throws Exception {
         try {
@@ -25,4 +30,13 @@ public class ChatController {
             throw new RuntimeException(ex);
         }
     }
+
+
+    //    -------------- WebSocket API ----------------
+    @MessageMapping("/sendMessage") //send message by client (app/sendMessage)
+    @SendTo("/topic/group") //pass this message to all subscriber of /topic/group url
+    public Message broadcastGroupMessage(@Payload Message message) {
+        return message;
+    }
+
 }
--- src/main/java/com/example/chat_app/service/KafkaConsumer.java
+++ src/main/java/com/example/chat_app/service/KafkaConsumer.java
@@ -2,13 +2,15 @@ package com.example.chat_app.service;
 
 import com.example.chat_app.config.KafkaConstants;
 import com.example.chat_app.domain.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
-//    @Autowired
-//    SimpMessagingTemplate template;
+    @Autowired
+    SimpMessagingTemplate template;
 
     @KafkaListener(
             topics = KafkaConstants.KAFKA_TOPIC,
@@ -16,7 +18,7 @@ public class KafkaConsumer {
     )
     public void listen(Message message) {
         System.out.println(message.getSender() + ":" + message.getContent());
-//            template.co("/topic/group", message);
+        template.convertAndSend("/topic/group", message);//send message to all subscriber of /topic/group websocket url
     }
 
 }
